a5af81e3b117674548225327dd17452d48817365
Oliver Puffer <pufferoliver@berkeley.edu>
2020-11-15 18:31:01 -0800

    Did a lot

--- sketch.js
+++ sketch.js
@@ -11,38 +11,47 @@ class GameState{
 }
 
 class Player{
-  direction; //0 = Left 1 = Right
+  speed = 2;
+  direction; //-1 = Left 1 = Right
   xPos;
   yPos;
   stress;
   standLeft;
   standRight;
+  walkLeft;
+  walkRight;
   higherStressStandL;
   higherStressStandR;
   higherStressWalkL;
   higherStressWalkR;
+  lowerStressStandL;
+  lowerStressStandR;
+  lowerStressWalkL;
+  lowerStressWalkR;
 
   constructor(){
     this.xPos = 100;
     this.yPos = 400;
-    this.stress = 1;
+    this.stress = 14;
     this.direction = 0;
     this.standLeft = this.createAnimation("stand","Left", this.stress);
-    /*
     this.standRight = this.createAnimation("stand","Right", this.stress);
+    this.walkLeft = this.createAnimation("walk","Left", this.stress);
+    this.walkRight = this.createAnimation("walk","Right", this.stress);
     this.higherStressStandL = this.createAnimation("stand","Left", this.stress + 1);;
     this.higherStressStandR = this.createAnimation("stand","Right", this.stress +1);;
     this.higherStressWalkL  = this.createAnimation("walk","Left", this.stress);;
     this.higherStressWalkR = this.createAnimation("walk","Left", this.stress);;
-    */
+    
   }
   displayPlayer(){
     if(this.direction == 0){
       animation(this.standLeft, this.xPos, this.yPos);
     } else {
-      animation(this.standRight,this.xPos, this.yPos)
+      animation(this.standRight, this.xPos, this.yPos)
     }
   }
+
   createAnimation(type, direction, stress){
     let numOfFrames;
     if(type == "stand")
@@ -66,26 +75,40 @@ class Player{
       `assets/${type}${direction}/sl${stress}/${(11 % numOfFrames) + 1}.png`);
 
   }
-  quereyMovement(){
+  queryMovement(){
     //MOVE RIGHT
     if (keyIsDown(68)){
-      //direction = 1;
+      this.direction = 1;
       console.log("RIGHT");
-    
+      this.xPos = this.xPos + this.speed;
+      animation(this.walkRight, this.xPos, this.yPos);
     }
     //MOVE LEFT
     else if (keyIsDown(65)){
-      console.log()
-      //direction = 0;
+      this.direction = -1;
       console.log("LEFT");
+      this.xPos = this.xPos - this.speed;
+      animation(this.walkLeft, this.xPos, this.yPos)
     }
     //MoveDown
     else if (keyIsDown(83)){
       console.log("DOWN");
+      this.yPos = this.yPos + this.speed;
+      if(this.direction = 1){
+        animation(this.walkRight, this.xPos, this.yPos)
+      } else {
+        animation(this.walkLeft, this.xPos, this.yPos)
+      }
     }
     //MOVE Up
     else if (keyIsDown(87)){
       console.log("UP");
+      this.yPos = this.yPos - this.speed;
+      if(this.direction = 1){
+        animation(this.walkRight, this.xPos, this.yPos)
+      } else {
+        animation(this.walkLeft, this.xPos, this.yPos)
+      }
     }
     //DO NOTHING
     else{
@@ -114,6 +137,6 @@ function setup() {
 function draw() {
   clear();
   background(200);
-  player.displayPlayer();
+  player.queryMovement();
   
 }
